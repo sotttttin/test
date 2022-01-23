fbf75455b695ebae679739857146e3185fed339b
LAPTOP-NFS8MR4T\rryyo <sotttttin>
2022-01-23 11:37:31 +0900

    初期ユーザー処理フォルダ作成

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //初期レコードを入力
+
+    }
+}
