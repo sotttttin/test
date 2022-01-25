8d426e1f22af3acf4179a07920330ef0a9867fcb
LAPTOP-NFS8MR4T\rryyo <sotttttin>
2022-01-26 00:25:09 +0900

    コード意味を追加記載

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 
-{!! Form::open() !!}
+{!! Form::open() !!}<!--Formの開始タグ-->
 
 <p>DAWNSNSへようこそ</p>
 
-{{ Form::label('e-mail') }}
-{{ Form::text('mail',null,['class' => 'input']) }}
-{{ Form::label('password') }}
+{{ Form::label('e-mail') }}　　　　　　　　　　　　　　　　<!--emailの欄を作成-->
+{{ Form::text('mail',null,['class' => 'input']) }}　　　 <!--emailの欄を作成-->
+{{ Form::label('password') }}                           <!--パスワード欄を作成-->
 {{ Form::password('password',['class' => 'input']) }}
 
-{{ Form::submit('ログイン') }}
+{{ Form::submit('ログイン') }}                           <!--ログインボタンを作成-->
 
-<p><a href="/register">新規ユーザーの方はこちら</a></p>
+<p><a href="/register">新規ユーザーの方はこちら</a></p>   <!--register.blade.phpに遷移-->
 
 {!! Form::close() !!}
 
