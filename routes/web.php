4c6cb35448775a4d640db9fa721f151a732a3fc6
LAPTOP-NFS8MR4T\rryyo <sotttttin>
2022-03-12 19:14:34 +0900

    createメソッドへのルーティングを記載

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::get('/added', 'Auth\RegisterController@added');
 
 //ログイン中のページ
 Route::get('/top','PostsController@index');
+Route::post('/top','PostsController@create');
 
 Route::get('/profile','UsersController@profile');
 
@@ -38,6 +39,3 @@ Route::get('/search','UsersController@index');
 
 Route::get('/follow-list','PostsController@index');
 Route::get('/follower-list','PostsController@index');
-
-
-
