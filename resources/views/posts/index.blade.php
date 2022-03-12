efb7d83c2a208f607330cb4b1899f4a488a3fb06
LAPTOP-NFS8MR4T\rryyo <sotttttin>
2022-03-12 19:13:25 +0900

    投稿画面のビューの記述を追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,4 +3,25 @@
 @section('content')
 <h2>機能を実装していきましょう。</h2>
 
+<div class='container'>
+{{Form::open()}}
+
+<div class='form-group'>
+{{Form::text('posts',null,['class'=>'form-control','placeholder'=>'何をつぶやこうか'])}}
+</div>
+
+{{Form::button('投稿',['type' => 'submit','class' => 'btn btn-success'])}}
+{{Form::close()}}
+</div>
+
+<table class='table table-hover'>
+  @foreach($list as $list)
+  <tr>
+    <td>{{ $list->id }}</td>
+    <td>{{ $list->posts }}</td>
+    <td>{{ $list->created_at }}</td>
+  </tr>
+  @endforeach
+</table>
+
 @endsection
